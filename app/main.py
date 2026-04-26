f63ab76ec4e37fc07ea4ac5791dd750988b9fa60
Renan Fernandes Marques <165963782+Renanmrqs@users.noreply.github.com>
2026-04-26 19:24:02 -0300

    feat: limpando linhas comentadas

--- app/main.py
+++ app/main.py
@@ -1,8 +1,5 @@
 from fastapi import FastAPI
-# , exceptions, HTTPException, security, Depends
 from app.routers import exercises, users, workouts
-# 
-
 
 app = FastAPI()
 
@@ -11,13 +8,11 @@ app.include_router(users.router)
 app.include_router(workouts.router)
 
 
-# READS
-# cria a rota padrao
 @app.get("/health")
 def health():
     return {"status": "ok"}
 
-#cria a rota de puxar o exercicio
+
 
 
 
