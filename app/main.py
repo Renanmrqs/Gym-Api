d350ac14e9086613845dc6f0e976e586af977cfd
Renanmrqs <renanmarques1923@gmail.com>
2026-05-18 17:38:06 -0300

    fix: ajustando o cors da api

--- app/main.py
+++ app/main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
 from app.routers import exercises, auth, workouts
 from app.ws import websocket
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "https://gym-api-08pc.onrender.com/",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],            # Allows specific origins
+    allow_credentials=True,           # Allows cookies and auth headers
+    allow_methods=["*"],              # Allows all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],              # Allows all headers
+)
+
 
 app.include_router(exercises.router)
 app.include_router(auth.router)
